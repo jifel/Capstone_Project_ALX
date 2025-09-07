a8fc06057aaf9985eb6a8a6bd3fd5f1fcc7d680e
elorm <jiffaelorm@gmail.com>
2025-09-07 23:08:41 +0000

    added auth token for api login test

--- QTrack/api/urls.py
+++ QTrack/api/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 from .views import IssueListCreateAPIView, IssueRetrieveUpdateDestroyAPIView
+from rest_framework.authtoken.views import obtain_auth_token
+
 
 urlpatterns = [
     # /api/issues/  -> list all issues (GET), create new (POST)
     path('issues/', IssueListCreateAPIView.as_view(), name='api_issue_list'),
+
+     # Token login endpoint
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
 
     # /api/issues/<id>/ -> retrieve, update, delete a single issue
     path('issues/<int:pk>/', IssueRetrieveUpdateDestroyAPIView.as_view(), name='api_issue_detail'),
